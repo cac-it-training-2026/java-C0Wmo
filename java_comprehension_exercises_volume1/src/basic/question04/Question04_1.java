1c7c0e29f121102a596f6ba72ba497282ce24241
takuriha <takuriha@C010423243.zipangu.cac.co.jp>
2026-04-16 11:55:36 +0900

    Question04の作成が完了しました。

--- java_comprehension_exercises_volume1/src/basic/question04/Question04_1.java
+++ java_comprehension_exercises_volume1/src/basic/question04/Question04_1.java
@@ -11,6 +11,11 @@ public class Question04_1 {
 	public static void main(String[] args) throws IOException {
 
 		// TODO:ここに実装
+		//		int[] ageGroup = { 12, 18, 24, 19, 20 };
+
+		int[] ageGroup = new int[5];
+
+		System.out.println("3番目の人の年齢は" + ageGroup[2] + "歳です。");
 
 	}
 
